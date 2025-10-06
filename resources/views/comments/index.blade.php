a23768f374cf6093c84bf60100db5aa0727b233a
User <nikolajevs04@gmail.com>
2025-10-06 11:23:29 +0300

    List

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -1,15 +1,16 @@
 {{-- display a list of comments in laravel blade view --}}
-@foreach ($comments as $comment)
-
 <ul>
+
+    @foreach ($comments as $comment)
     <li>{{$comment->content}}</li>
     <form action="{{ route('comment.destroy', $comment->id) }}" method="POST">
         @csrf
         @method("DELETE")
         <button type="submit" class="btn-danger">Delete</button>
     </form>
+    @endforeach
 </ul>
-@endforeach
+
 
 <a href="/comments/create">Make Comment</a>
 {{-- create a link back to create form --}}
